45d7b5fa8f1b9d84f47bb087fccaa11821abd2b0
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-05-13 20:36:06 -0400

    Themes

--- HunterFreemanDev.ClassLibrary/Theme/ThemeFacts.cs
+++ HunterFreemanDev.ClassLibrary/Theme/ThemeFacts.cs
@@ -25,14 +25,14 @@ public static class ThemeFacts
 
     public static readonly ThemeRecord HfdDarkThemeDefaultContrast = new ThemeRecord(Guid.NewGuid(),
         "Hfd Dark Theme Default Contrast",
-        "hfd_hfd-light-theme-high-contrast",
+        "hfd_hfd-dark-theme-default-contrast",
         "The default light theme for the IDE with default contrast.",
         ThemeContrastKind.DefaultContrast,
         ThemeColorPaletteKind.Light);
 
     public static readonly ThemeRecord HfdDarkThemeHighContrast = new ThemeRecord(Guid.NewGuid(),
         "Hfd Dark Theme High Contrast",
-        "hfd_hfd-dark-theme-default-contrast",
+        "hfd_hfd-dark-theme-high-contrast",
         "The default dark theme for the IDE with high contrast.",
         ThemeContrastKind.DefaultContrast,
         ThemeColorPaletteKind.Light);
